59d9ad267347bf7bfcf58fb1f47df702b0b25f4d
ArisayMella <163840802+ArisayMella@users.noreply.github.com>
2024-06-20 11:58:41 +0200

    updated

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,21 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
-import '../scss/styles.scss';
+/*import '../scss/styles.scss';
+
+const titleElement = document.getElementById('title');
+
+console.dir(titleElement);
+
+console.dir(titleElement.parentElement);
+console.dir(titleElement.nextElementSibling);*/
+
+const startPointElement = document.getElementById('start-point');
+
+console.dir(startPointElement);
+
+console.log(`Soy un ${startPointElement.tagName} con el id start-point`);
+
+const firstListElement = document.getElementById('first-list');
+const secondListElement = document.getElementById('second-list');
+
+console.dir(firstListElement.textContent);
+console.dir(secondListElement.textContent);
